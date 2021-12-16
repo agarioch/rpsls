30bda1e8e155558983e6cb013222b66306e8ee5f
Alistair <alistairgarioch@gmail.com>
2021-12-16 18:19:12 +0000

    Refactor(rounds): refactor rounds api to check for game winner

--- src/helpers/api/rounds.ts
+++ src/helpers/api/rounds.ts
@@ -1,3 +1,12 @@
-export const playGame = () => {
-  return "player wins!";
+import { Gesture } from "../../../types";
+import { gestures, gestureBeats } from "../pages/gestures";
+
+export const playGame = ({ playerGesture }: { playerGesture: Gesture }) => {
+  const computerGesture = gestures[Math.floor(Math.random() * gestures.length)];
+
+  if (gestureBeats[playerGesture].includes(computerGesture))
+    return "player wins!";
+  if (gestureBeats[computerGesture].includes(playerGesture))
+    return "computer wins!";
+  return "draw!";
 };
