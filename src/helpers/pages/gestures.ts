946281badcbeafe395425d7a9db5aa3bf44ea4df
Alistair <alistairgarioch@gmail.com>
2021-12-16 19:20:10 +0000

    Fix(gestures): fix gestureBeats logic

--- src/helpers/pages/gestures.ts
+++ src/helpers/pages/gestures.ts
@@ -1,13 +1,13 @@
 import { Gesture } from "../../../types";
 
-const gestures: Gesture[] = ["rock", "paper", "scissors", "lizard", "spock"];
+const gestures: Gesture[] = ["scissors", "paper", "rock", "lizard", "spock"];
 
 const gestureBeats = {};
 // create mapping of gesture to gestures they beat e.g. rock: [spock, paper]
 gestures.forEach((gesture, index) => {
   const next = (index + 1) % gestures.length;
-  const prior = index - 1;
-  gestureBeats[gesture] = [gestures.at(prior), gestures.at(next)];
+  const third = (index + 3) % gestures.length;
+  gestureBeats[gesture] = [gestures.at(next), gestures.at(third)];
 });
 
 export { gestures, gestureBeats };
